c92bbec0c9ae87946a40da008ea87954bd54172a
Juan Diego <juandiego90123@gmail.com>
2022-04-23 18:07:11 -0500

    Feat: add function dropdown menu mobile

--- src/components/Header/Mobile.tsx
+++ src/components/Header/Mobile.tsx
@@ -1,19 +1,30 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import {ReactComponent as MindCodeLogo} from '../../assets/logo.svg'
 import {ReactComponent as MenuIcon} from '../../assets/menu-mobile.svg'
+import Navigator from '../Navigator'
 
 const HeaderMobile:FC = () => {
+
+  const [open, setOpen] = useState<boolean>(false)
+
+  const toggleMenu = () => setOpen(!open)
+
   return (
-    <header className='header header-mobile'>
+    <header className={`header header-mobile ${open?'open':'close'}`}>
       <div className='header-mobile__wrapper'>
         <div className='logo'>
           <MindCodeLogo/>
         </div>
+        
         <div className='menu'>
-          <div className='menu-container'>
+          <div className='menu-trigger' onClick={toggleMenu}>
             <MenuIcon/>
           </div>
         </div>
+      </div>
+
+      <div className='menu-container'>
+          <Navigator/>
       </div>
     </header>
   )
